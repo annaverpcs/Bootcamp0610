f31ad9d3177f70715c47bb22ea4a0a9db9afcb67
Marianna <mariannavozniuk@gmail.com>
2024-07-17 00:25:20 -0500

    Marianna: creating own method

---
+++ src/test/java/definitions/MariannaXPathesLibrary.java
@@ -0,0 +1,14 @@
+package definitions;
+
+public class MariannaXPathesLibrary {
+    public static String sMediCenterUrlV1 = "https://medicenter-qa1.vercel.app/";
+
+    public static String sMediCenterUrlV2 = "https://medicenter-qa2.vercel.app/";
+
+    public static String Google = "https://www.google.com/";
+
+    public static String sEmailTextField = "//input[@id='email']";
+
+    public static String sPasswordTextField = "//input[@id='password']";
+
+}
